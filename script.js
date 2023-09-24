01cbef3d67495e53c905403581774c907ff99a96
T0YA <toyaAoi@users.noreply.github.com>
2023-09-24 08:08:46 +0530

    p

---
+++ script.js
@@ -0,0 +1,31 @@
+const swtch = document.getElementsByClassName("switch");
+const toggler = document.getElementById("toggler");
+const basicPrice = document.getElementById("basic-price");
+const professionalPrice = document.getElementById("professional-price");
+const masterPrice = document.getElementById("master-price");
+
+let plan = 1;
+
+const monthly = function () {
+  toggler.style.transform = "translate(90%)";
+  basicPrice.innerHTML = "19.99";
+  professionalPrice.innerHTML = "24.99";
+  masterPrice.innerHTML = "39.99";
+  plan = 0;
+};
+
+const anually = function () {
+  toggler.style.transform = "translate(0)";
+  basicPrice.innerHTML = "199.99";
+  professionalPrice.innerHTML = "249.99";
+  masterPrice.innerHTML = "399.99";
+  plan = 1;
+};
+
+swtch[0].addEventListener("click", function () {
+  if (plan === 1) {
+    monthly();
+  } else if (plan === 0) {
+    anually();
+  }
+});
